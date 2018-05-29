749114fba22193506ee8f709f456e290f58c31b0
Therese Sturesson <thestu@student.chalmers.se>
2018-05-29 22:11:00 +0200

    added an abstract class and a player class

--- core/src/se/tthera/game/model/Character.java
+++ core/src/se/tthera/game/model/Character.java
@@ -1,4 +1,29 @@
 package se.tthera.game.model;
 
-public class Character {
+public abstract class Character {
+
+    protected int baseDamage;
+    protected int maxHealth;
+    protected int health;
+    protected Facing facing;
+    protected Position position;
+
+    public Character(int baseDamage, int maxHealth, int health, Facing facing, Position position) {
+        this.baseDamage = baseDamage;
+        this.maxHealth = maxHealth;
+        this.health = health;
+        this.facing = facing;
+        this.position = position;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+
+
 }
--- core/src/se/tthera/game/model/PlayerCharacter.java
+++ core/src/se/tthera/game/model/PlayerCharacter.java
@@ -1,4 +1,20 @@
 package se.tthera.game.model;
 
-public class PlayerCharacter {
+public class PlayerCharacter extends Character {
+
+    public PlayerCharacter(int baseDamage, int maxHealth, int health, Facing facing, Position position) {
+        super(baseDamage, maxHealth, health, facing, position);
+    }
+
+    public void move(Position pos) {
+
+
+    }
+
+    public void death() {
+
+        
+    }
+
+
 }
