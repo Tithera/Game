74624a787772039e49e5ea548e0db1a88a7da43e
Therese Sturesson <thestu@student.chalmers.se>
2018-05-29 21:44:54 +0200

    added a facing

--- core/src/se/tthera/game/model/Facing.java
+++ core/src/se/tthera/game/model/Facing.java
@@ -1,4 +1,15 @@
 package se.tthera.game.model;
 
-public class Facing {
+/**
+ * @author Therese
+ *
+ * A class that defines an enum for which direction you are facing.
+ */
+
+public enum Facing {
+    NORTH,
+    RIGHT,
+    SOUTH,
+    WEST,
+    NONE
 }
